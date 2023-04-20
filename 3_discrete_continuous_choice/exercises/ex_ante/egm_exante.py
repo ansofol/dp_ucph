2646742f5de5173c51d35ad9e2ef7da1d7517ebe
ansofol <ansofol98@gmail.com>
2023-04-20 10:30:06 +0200

    a bunch of broadcasting issues in egm exercises

--- 3_discrete_continuous_choice/exercises/ex_ante/egm_exante.py
+++ 3_discrete_continuous_choice/exercises/ex_ante/egm_exante.py
@@ -5,8 +5,8 @@ import tools
 
 
 def EGM(sol,t,par):
-    sol = EGM_loop(sol,t,par) 
-    #sol = EGM_vec(sol,t,par) 
+    #sol = EGM_loop(sol,t,par) 
+    sol = EGM_vec(sol,t,par) 
     return sol
 
 def EGM_loop (sol,t,par):
@@ -14,6 +14,7 @@ def EGM_loop (sol,t,par):
 
         # Fill in:
         # Hint: Same procedure as in 02_EGM.ipynb
+        interp = lambda m: tools.interp_linear_1d(sol.m[t+1,:], sol.c[t+1,:], m)
         
         # Future m and c
         if t+1<= par.Tr: # No pension in the next period
@@ -22,7 +23,10 @@ def EGM_loop (sol,t,par):
         else:
             fac = par.G*par.L[t]
 
+        m_next = fac*par.R*a + par.xi_vec
+        
         # Future expected marginal utility
+        mu_next = util.marg_util(m_next)
 
         # Current C and m
         #sol.c[t,i_a+1]=
@@ -30,9 +34,29 @@ def EGM_loop (sol,t,par):
 
     return sol
 
-def EGM_vec (sol,t,par):
-
-    #Fill in:
+def EGM_vec (sol,t,par): 
     
+    interp = lambda m: tools.interp_linear_1d(sol.m[t+1,:], sol.c[t+1,:], m)
+
+    
+    if t+1<= par.Tr: # No pension in the next period
+        fac = par.G*par.L[t]*par.psi_vec[:,np.newaxis] # Trick to ease notation and calculations
+
+    else:
+        fac = par.G*par.L[t]
+
+
+    m_next = ((1/fac)*par.R*par.grid_a[t, np.newaxis] + par.xi_vec[:,np.newaxis]).ravel()
+    c_next = interp(m_next) # something is wrong here :)
+    c_next_adj = c_next/(np.tile(fac, par.Na).ravel())
+
+    ws = (np.tile(par.w, par.Na))
+    mu_next = util.marg_util(c_next_adj, par)
+    Emu_next = (ws*mu_next).reshape(par.Nshocks,par.Na).sum(axis=0)
+    c = util.inv_marg_util(Emu_next, par)
+    
+
+    sol.c[t, 1:] = c
+    sol.m[t, 1:] = par.grid_a[t] - c
 
     return sol
